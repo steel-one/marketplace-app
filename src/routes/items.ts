cea0bd835f942a2849162a0fbf3786ac68851011
ViacheslavDenisenko <vdenisenko@centrak.com>
2024-11-06 13:10:23 +0400

    fetchItems

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,33 +1,94 @@
+import fastifyCompress from '@fastify/compress';
 import axios from 'axios';
 import { FastifyInstance } from 'fastify';
 import Redis from 'ioredis';
 import { authenticate } from '../utils/authenticate';
+
+interface IResponse {
+  data: IData[];
+}
+
+interface IData {
+  market_hash_name: string;
+  currency: string;
+  suggested_price: number;
+  item_page: string;
+  market_page: string;
+  min_price: number;
+  max_price: number;
+  mean_price: number;
+  quantity: number;
+  created_at: number;
+  updated_at: number;
+}
 
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT || 6379),
 });
 
+async function fetchItems(tradable: boolean) {
+  try {
+    const response: IResponse = await axios.get(
+      'https://api.skinport.com/v1/items',
+      {
+        params: {
+          app_id: process.env.DEFAULT_APP_ID,
+          currency: process.env.DEFAULT_CURRENCY,
+          tradable,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Failed to fetch items with tradable=${tradable}:`, error);
+    throw error;
+  }
+}
+
 export default async function itemRoutes(fastify: FastifyInstance) {
+  fastify.register(fastifyCompress);
+
   fastify.get('/items', async (request, reply) => {
+    const { page = 1, limit = 50 } = request.query as {
+      page?: number;
+      limit?: number;
+    };
+
     const user = await authenticate(request, reply);
     if (!user) return;
 
-    const cachedItems = await redis.get('items');
+    const cacheKey = `items:${page}:${limit}`;
+    const cachedItems = await redis.get(cacheKey);
     if (cachedItems) {
       return JSON.parse(cachedItems);
     }
 
-    const response = await axios.get('https://api.skinport.com/items', {
-      params: { app_id: 'default', currency: 'default' },
-    });
-    const items = response.data.map((item: any) => ({
-      name: item.name,
-      tradable_price: item.tradable_price,
-      non_tradable_price: item.non_tradable_price,
-    }));
+    const [nonTradableResponse, tradableResponse] = await Promise.all([
+      fetchItems(false),
+      fetchItems(true),
+    ]);
 
-    await redis.set('items', JSON.stringify(items), 'EX', 3600); // Кешируем на 1 час
-    return items;
+    const nonTradableItems = nonTradableResponse.reduce(
+      (acc: Record<string, any>, item: any) => {
+        acc[item.market_hash_name] = item.min_price;
+        return acc;
+      },
+      {},
+    );
+
+    const allItems = tradableResponse.map((tradableItem: any) => {
+      return {
+        name: tradableItem.market_hash_name,
+        tradable_price: tradableItem.min_price,
+        non_tradable_price:
+          nonTradableItems[tradableItem.market_hash_name] || null,
+      };
+    });
+
+    const paginatedItems = allItems.slice((page - 1) * limit, page * limit);
+    await redis.set(cacheKey, JSON.stringify(paginatedItems), 'EX', 3600);
+
+    return paginatedItems;
   });
 }
